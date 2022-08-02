f044bb2f60e75b300034a1a3371e2645ac3d2793
--unset <gawrysjak@gmail.com>
2022-08-02 19:11:23 +0200

    added simple main menu

---
+++ space-shooter/Assets/Content/Scripts/MMBackGroundController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MMBackGroundController : MonoBehaviour
+{
+
+    public GameObject bg1, bg2, bg3;
+    public List<GameObject> backgroundObj;
+    float bgScrollingSpeed = 1.1f;
+
+    private void FixedUpdate()
+    {
+        backgroundMovment();
+    }
+
+    private void backgroundMovment()
+    {
+        bg1.transform.position -= new Vector3(bgScrollingSpeed * Time.deltaTime, 0f);
+        bg2.transform.position -= new Vector3(bgScrollingSpeed * Time.deltaTime, 0f);
+        bg3.transform.position -= new Vector3(bgScrollingSpeed * Time.deltaTime, 0f);
+
+        if (bg1.transform.position.x < -14f)
+        {
+            var bgObj = Instantiate(backgroundObj[(int)Random.Range(0, backgroundObj.Count)], new Vector3(15f, 0), Quaternion.identity);
+
+            Destroy(bg1);
+
+            bg1 = bg2;
+            bg2 = bg3;
+            bg3 = bgObj;
+        }
+
+    }
+}
--- space-shooter/Assets/Content/Scripts/MainMenu/MainMenuManager.cs
+++ space-shooter/Assets/Content/Scripts/MainMenu/MainMenuManager.cs
@@ -2,13 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
+
+    public Image logo;
+    Color logoColor;
     // Start is called before the first frame update
     void Start()
     {
-        
+        logoColor = logo.color;
+        StartCoroutine(logoAnim());
     }
 
     // Update is called once per frame
@@ -20,5 +25,27 @@ public class MainMenuManager : MonoBehaviour
     public void StartGame()
     {
         SceneManager.LoadScene("GameScene");
+    }
+
+    public void RestHiScore()
+    {
+        PlayerPrefs.SetInt("SS-score", 0);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    private IEnumerator logoAnim()
+    {
+        while (true)
+        {
+            yield return new WaitForSecondsRealtime(1.5f);
+            logo.color = Color.clear;
+            yield return new WaitForSecondsRealtime(0.8f);
+            logo.color = logoColor;
+        }
+        
     }
 }
